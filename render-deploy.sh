ec9525b5a24b591a0b6a9fa8a6584c35f749220d
Joao lucas <joaolucaspereira2002@gmail.com>
2025-07-30 15:54:58 -0300

    g

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e  # Encerra o script ao primeiro erro
 
-
 # Executa as migrações do banco de dados com Flask-Migrate
-python run flask --app src.app db upgrade
-python run gunicorn src.wsgi:app
+flask --app src.app db upgrade
+
+# Inicia o servidor com Gunicorn
+exec gunicorn src.wsgi:app
