94d4a1dc0fe2849281f18188507820658958c320
Joao lucas <joaolucaspereira2002@gmail.com>
2025-07-30 16:10:47 -0300

    adiciona requirements.txt com dependências

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,8 +1,9 @@
+
 #!/bin/bash
 set -e  # Encerra o script ao primeiro erro
 
-# Executa as migrações do banco de dados com Flask-Migrate
+# Migrações com Flask-Migrate
 flask --app src.app db upgrade
 
-# Inicia o servidor com Gunicorn
-exec gunicorn src.wsgi:app
+# Inicia com Gunicorn
+gunicorn src.wsgi:app
