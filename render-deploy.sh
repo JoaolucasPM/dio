6aa14ac0e99babcd36f5c829a607040354138477
Joao lucas <joaolucaspereira2002@gmail.com>
2025-07-30 15:51:46 -0300

    'final'

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e  # Encerra o script ao primeiro erro
 
-# Instala as dependências do projeto
-pip install -r requirements.txt
 
 # Executa as migrações do banco de dados com Flask-Migrate
-flask --app src.app db upgrade
-
-# Inicia o servidor com Gunicorn apontando para o app WSGI
-exec gunicorn src.wsgi:app
-echo "Pulando instalação de requirements.txt"
+python run flask --app src.app db upgrade
+python run gunicorn src.wsgi:app
